2437c69fbcd016cbddc103214f3a1db2ca3d8158
Jehad-Amjad <gehadamgad501@gmail.com>
2024-09-15 17:28:46 +0300

    task jehad

--- include/Admin.h
+++ include/Admin.h
@@ -1,13 +1,22 @@
 #ifndef ADMIN_H
 #define ADMIN_H
 #include <iostream>
-#include <Employee.h>
+#include "Employee.h"
+#include <string>
+#include <vector>
 using namespace std;
 
 class Admin :public Employee
 {
-    public:
+private:
+    vector<Employee>employees;
+public:
         Admin();
+        //methods
+        void addEmployee(Employee&employee);
+        Employee* searchEmployee(int id);
+        void editEmployee(int id ,string name ,string password ,double salary);
+        void listEmployee();
 
 };
 
--- src/Admin.cpp
+++ src/Admin.cpp
@@ -1,8 +1,51 @@
 #include "Admin.h"
+#include "Employee.h"
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+Admin::Admin() : Employee(){
 
-Admin::Admin()
-{
-    //ctor
 }
+void Admin::addEmployee(const Employee& employee){
+employees.push_back(employee);
+}
+
+Employee* Admin::searchEmployee(int id){
+    for (Employee& employee : employees)
+    {
+        if (employee.Getid() == id)
+        {
+            return &employee;
+        }
+    }
+    return nullptr;
+
+}
+
+void Admin::editEmployee(int id, const string& name ,const string& password ,double salary)
+{
+   Employee* emp =searchEmployee(id);
+   if (emp)
+   {
+       emp->Setname(name);
+       emp->Setpassword(password);
+       emp->setSalary(salary);
+   }
+   else{
+    cout <<"Not Found!"<<endl;
+   }
+
+}
+
+void Admin::listEmployee()const
+{
+     for (const Employee& employee : employees)
+     {
+         cout <<"ID: "<<employee.Getid()<<", Name: " << employee.Getname()<<", Salary: "<<employee.GetSalary() << endl;
+     }
+
+}
+
 
 
